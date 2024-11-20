c654569789530cc2d9fd02dba066e7e03e68df07
Brun0hs <bruno.henri.souza100@gmail.com>
2024-11-20 14:03:28 -0300

    Add files via upload

---
+++ ProjetoFinal/src/fp/brn/controle/ControleContato.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package fp.brn.controle;
+import fp.brn.modelos.classes.Contato;
+import fp.brn.modelos.interfaces.IContatoCRUD;
+import java.util.List;
+/**
+ *
+ * @author Bruno
+ */
+public class ControleContato implements IContatoCRUD{
+
+    @Override
+    public void incluir(Contato pessoa) throws Exception {
+        throw new Exception("controle sendo executado - incluir");
+    }
+
+    @Override
+    public void alterar(Contato pessoa) throws Exception {
+        throw new Exception("controle sendo executado - alterar");
+    }
+
+    @Override
+    public void excluir(int identificador) throws Exception {
+        throw new Exception("controle sendo executado - excluir");
+    }
+
+    @Override
+    public Contato consultar(int identificador) throws Exception {
+        throw new Exception("controle sendo executado - consultar");
+    }
+
+    @Override
+    public Contato consultar(String nome) throws Exception {
+        throw new Exception("controle sendo executado - consultar");
+    }
+
+    @Override
+    public List<Contato> ListagemDeContatos() throws Exception {
+        throw new Exception("controle sendo executado - listagem");
+    }
+    
+}
